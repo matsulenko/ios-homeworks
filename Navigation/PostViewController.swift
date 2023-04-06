462216057017258be90eed4a6b0b1cb917c958b1
Andrey Matsulenko <matsulenko@mail.ru>
2023-04-06 22:37:04 +0200

    Homework 1.3 is done

---
+++ Navigation/PostViewController.swift
@@ -0,0 +1,48 @@
+//
+//  PostViewController.swift
+//  Navigation
+//
+//  Created by Matsulenko Andrey on 06.04.2023.
+//
+
+import UIKit
+
+struct Post {
+    var title: String
+}
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+
+        let infoViewController = InfoViewController()
+
+        infoViewController.title = "Инфо"
+
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "info.circle"), style: .plain, target: self, action: #selector(buttonPressed(_:)))
+
+        view.backgroundColor = .systemGray4
+    }
+
+    @objc func buttonPressed(_ sender: UIBarButtonItem) {
+
+        let infoViewController = InfoViewController()
+
+        present(infoViewController, animated: true)
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
